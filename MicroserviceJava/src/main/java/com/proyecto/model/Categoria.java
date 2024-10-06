9d252a68362ca6e1b62081f962968825687d30a6
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-10-06 09:14:47 -0500

    Add files via upload

---
+++ MicroserviceJava/src/main/java/com/proyecto/model/Categoria.java
@@ -0,0 +1,34 @@
+package com.proyecto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_categoria;
+
+    private String nombre;
+
+    // Getters y Setters
+    public Long getId_categoria() {
+        return id_categoria;
+    }
+
+    public void setId_categoria(Long id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
