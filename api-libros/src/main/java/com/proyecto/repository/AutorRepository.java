46f8258ab0153e4704280e5734faff7925afc4f5
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-10-06 08:59:42 -0500

    Delete api-libros directory

--- api-libros/src/main/java/com/proyecto/repository/AutorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.proyecto.model.Autor;
-
-public interface AutorRepository extends JpaRepository<Autor, Long> {
-}
--- api-libros/src/main/java/com/proyecto/service/LibroService.java
+++
@@ -1,4 +0,0 @@
-package com.proyecto.service;
-
-public class LibroService {
-}
